26407b79ab67411e22ae916226a1b4df82a9f3dd
Rafiad <rafiadhoseen@gmail.com>
2025-06-21 21:27:38 +0600

    Add Yearly fuction..

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 function toggleBilling(view) {
-    // Toggle Button
-    const toggleButtons = document.querySelectorAll('.toggle-option');
-    toggleButtons.forEach(button => {
-        if(button.classList.contains(view)) {
-            button.classList.add('active');
-        } else {
-            button.classList.remove('active');
-        }
-    });
+  // Toggle Button
+  const toggleButtons = document.querySelectorAll(".toggle-option");
+  toggleButtons.forEach((button) => {
+    if (button.classList.contains(view)) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
 
-    // Cards
-    const cards = document.querySelectorAll('.card');
-    cards.forEach(card => {
-        if(card.classList.contains(view)) {
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
-        }
-    });
+  // Cards
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    if (card.classList.contains(view)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
 }
 
 // Default view
-window.onload = function() {
-    toggleBilling('Monthly');
-}
+window.onload = function () {
+  toggleBilling("Monthly");
+};
